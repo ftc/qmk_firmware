83d8021b9101ef428cc9449baecd074b64137689
Shawn Meier <shawn.meier@colorado.edu>
2023-02-24 14:00:25 -0700

    work on scroll

--- keyboards/ploopyco/trackball/keymaps/ploopy0/keymap.c
+++ keyboards/ploopyco/trackball/keymaps/ploopy0/keymap.c
@@ -16,6 +16,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "trackball.h"
+#define PLOOPY_DRAGSCROLL_MULTIPLIER 0.05
+#define PLOOPY_DRAGSCROLL_MOMENTARY 
+#define PLOOPY_DRAGSCROLL_INVERT
 
 // safe range starts at `PLOOPY_SAFE_RANGE` instead.
 //
@@ -51,6 +55,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT( /* Base */
         KC_BTN1, KC_BTN3, KC_BTN2,
-          UI_CPY, UI_PST
+          DRAG_SCROLL, UI_PST
     ),
 };
